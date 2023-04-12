9e851ac5d9a709882ef8fc2e1437d76551886bc7
ABiviano13 <abiviano0@outlook.com>
2023-04-12 17:39:48 +0200

    CORRECT

---
+++ database/migrations/2023_04_12_152958_update2_trains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('Orario di Partenza');
+            $table->time('Orario di Arrivo');
+            $table->string('In orario', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
+    }
+};
